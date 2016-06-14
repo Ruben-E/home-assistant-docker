52a4348a1a0d7d413a215f2885f91fe45d42fcfd
Ruben Ernst <rubenernst@gmail.com>
2016-06-14 19:36:24 +0200

    Extracted geo data to environment variables

--- configuration/run.sh
+++ configuration/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sed -i -e 's@_MYSENSORS_GATEWAY_IP_@'"$MYSENSORS_GATEWAY_IP"'@g' /config/configuration.yaml
 sed -i -e 's@_MYSENSORS_GATEWAY_PORT_@'"$MYSENSORS_GATEWAY_PORT"'@g' /config/configuration.yaml
+sed -i -e 's@_HOME_LATITUDE_@'"$HOME_LATITUDE"'@g' /config/configuration.yaml
+sed -i -e 's@_HOME_LONGITUDE_@'"$HOME_LONGITUDE"'@g' /config/configuration.yaml
 
 python -m homeassistant --config /config
